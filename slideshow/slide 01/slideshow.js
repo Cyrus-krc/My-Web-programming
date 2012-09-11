696c9838386b07cc13966063978d5b93bde35a46
cyrus-krc <koorosh.krc@gmail.com>
2012-09-11 14:43:05 +0430

    add thumbnails and fix lists

--- slideshow/slide 01/slideshow.js
+++ slideshow/slide 01/slideshow.js
@@ -3,17 +3,22 @@ window.onload=function(){
 	var pagewidth=innerWidth;
 	var train = slideshow.getElementsByClassName('train').item(0);
 	
+	rnd=function(a,b){
+		return Math.floor(Math.random()*(b-a+1))+a;
+	}
 	go2slide = function (n) {
-		slideshow.style.width=pagewidth;
+		slideshow.style.width=pagewidth+'px';
 		train.style.left=(-pagewidth*n)+'px';
 	}
 
 	var lists = slideshow.getElementsByClassName('btns').item(0).getElementsByTagName('li');
 	for (var i=0; i<lists.length; i++) {
 		(function(j){
+			lists.item(j).style.webkitTransform='rotate('+rnd(-25,25)+'deg)';
 			lists.item(j).onclick=function(){
 				go2slide(j);
 			}
 		})(i);
 	}
+	
 }

d89ce58d142b2d24ddb49b4df86f9665d6dcdcae
cyrus-krc <koorosh.krc@gmail.com>
2012-09-18 20:36:36 +0330

    fix auto play

--- slideshow/slide 01/slideshow.js
+++ slideshow/slide 01/slideshow.js
@@ -2,6 +2,7 @@ window.onload=function(){
 	var slideshow = document.getElementsByClassName('slideshow').item(0);
 	var pagewidth=innerWidth;
 	var train = slideshow.getElementsByClassName('train').item(0);
+	var currentSlide = 0;
 	
 	rnd=function(a,b){
 		return Math.floor(Math.random()*(b-a+1))+a;
@@ -13,7 +14,9 @@ window.onload=function(){
 		if(n>lists.length-1) n=0;
 		train.style.left=(-pagewidth*n)+'px';
 		lists.item(currentSlide).className = '';
+		lists.item(currentSlide).style.webkitTransform='rotate('+rnd(-10,10)+'deg)';
 		lists.item(n).className = 'active';
+		lists.item(n).style.webkitTransform='rotate('+rnd(0,0)+'deg)';		
 		currentSlide=n;
 	}
 	
@@ -21,12 +24,11 @@ window.onload=function(){
 			go2slide(currentSlide+1);
 		}
 
-
 	var lists = slideshow.getElementsByClassName('btns').item(0).getElementsByTagName('li');
 	for (var i=0; i<lists.length; i++) {
 		(function(j){
-			lists.item(j).style.webkitTransform='rotate('+rnd(-25,25)+'deg)';
-			lists.item(j).style.mozTransform='rotate('+rnd(-25,25)+'deg)';
+			lists.item(j).style.webkitTransform='rotate('+rnd(-10,10)+'deg)';
+			lists.item(j).style.mozTransform='rotate('+rnd(-10,10)+'deg)';
 			lists.item(j).onclick=function(){
 				go2slide(j);
 			}

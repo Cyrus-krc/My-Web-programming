7c03792207968ea38f7f3f013cfd45b7c07ab827
cyrus-krc <koorosh.krc@gmail.com>
2012-09-22 18:27:13 +0330

    fix problems finnaly

--- slideshow/slide 01/index.php
+++ slideshow/slide 01/index.php
@@ -9,19 +9,20 @@
 </head>
 <body>
 	<div class="container">
+		<div class="cover">
+		</div>
 		<div class="slideshow">
-			<div class="cover">
+			
 			<div class="train">
 				<?php
 					$images_path='./images';
+					$images_thumbnails_path = "./images/thumbnail";
 					$images = scandir($images_path);
-					$slidesLen = 0;
 						foreach($images as $img){
 							$img_arr = explode('.',$img);
 							$img_type = strtolower( end($img_arr) );
 							if($img_type=='jpg'){
 								echo "<div style=\"background-image:url('$images_path/$img');\"></div>";
-								$slidesLen++;
 							}
 						}
 				?>
@@ -30,12 +31,18 @@
 			<div class="next"><div class="nxt"></div></div>
 			<ul class="btns">
 				<?php
-					for($li;$li<$slidesLen;$li++){
-						echo "<li style=\"background-image:url('$images_path/$img');\"></li>";
+					$images_thumbnails = scandir($images_thumbnails_path);
+					foreach($images_thumbnails as $thumb_img){
+						$img_arr = explode('.',$thumb_img);
+						$img_type = strtolower( end($img_arr) );
+						if($img_type=='jpg'){
+							echo "<li style=\"background-image:url('$images_thumbnails_path/$thumb_img');\"></li>";
+						}
 					}
 				?>
 			</ul>
-		</div>
+		
 	</div>
+</div>
 </body>
 </html>
